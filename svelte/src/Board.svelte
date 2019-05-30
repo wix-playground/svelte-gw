7039b46d5917a0232aa9dc076a333d1e081eeced
Andrei Sierkov <andriisi@wix.com>
2019-05-30 14:45:09 +0300

    Passing Data Through Props

---
+++ svelte/src/Board.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Square from './Square.svelte';
+</script>
+<style>
+  .board-row:after {
+    clear: both;
+    content: "";
+    display: table;
+  }
+</style>
+<div class="game-board">
+  <div class="board-row">
+    <Square value={0} />
+    <Square value={1} />
+    <Square value={2} />
+  </div>
+  <div class="board-row">
+    <Square value={3} />
+    <Square value={4} />
+    <Square value={5} />
+  </div>
+  <div class="board-row">
+    <Square value={6} />
+    <Square value={7} />
+    <Square value={8} />
+  </div>
+</div>
---
+++ svelte/src/Game.svelte
@@ -0,0 +1,37 @@
+<script>
+  import Board from './Board.svelte';
+</script>
+<style>
+  .game-logo {
+    display: flex;
+    margin: 0 auto 20px;
+    width: 350px;
+    font-size: 30px;
+    line-height: 50px;
+  }
+  .game-logo img {
+    width: 50px;
+    height: 50px;
+    margin-right: 10px;
+  }
+	.game {
+	  margin: auto;
+	  width: 350px;
+    display: flex;
+    flex-direction: row;
+  }
+  .game-info {
+    margin-left: 20px;
+  }
+</style>
+
+<div class="game-logo">
+  <img src="https://image.flaticon.com/icons/svg/1527/1527318.svg" alt="Tic Tac Toe"> Tic Tac Toe
+</div>
+<div class="game">
+  <Board />
+  <div class="game-info">
+    <div><!-- TODO --></div>
+    <ol><!-- TODO --></ol>
+  </div>
+</div>
